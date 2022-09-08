09dd13f89180bbab619869ea82c480794676412d
Sarbajit Nandy <sarbajitnandey@gmail.com>
2022-09-08 22:14:52 +0530

    App2 is modified with async

--- app2.js
+++ app2.js
@@ -1,10 +1,13 @@
-const app2 = () => {
-  return new Promise((re, rj) => {
+const { dbConnection } = require("./db");
+
+const app2 = async () => {
+  const appPromise = new Promise((re, rj) => {
     setTimeout(() => {
       console.log("App2 setup is done");
       re(8000);
     }, 2000);
   });
+  return Promise.all([dbConnection(), appPromise]);
 };
 
 module.exports = { app2 };
